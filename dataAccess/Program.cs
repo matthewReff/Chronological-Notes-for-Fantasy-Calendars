4d788538d841d68af80274b7c80558457584f3af
Geniusammyr <geniusammyralltheothernamesare@gmail.com>
2019-05-30 00:06:13 -0600

    Add project files.

---
+++ dataAccess/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace dataAccess
+{
+    class Program
+    {
+        
+    }
+}
---
+++ temp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tempDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
